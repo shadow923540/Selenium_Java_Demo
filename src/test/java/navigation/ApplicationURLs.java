3e419f2adf351f4d14e4bd7ef9e12d4b386a96fe
Zuczkows <pz921@wp.pl>
2019-11-03 21:23:16 +0100

    add new Sign up url

--- src/test/java/navigation/ApplicationURLs.java
+++ src/test/java/navigation/ApplicationURLs.java
@@ -3,4 +3,6 @@ package navigation;
 public class ApplicationURLs {
     public static final String APPLICATION_URL= "https://trello.com";
     public static final String LOGIN_URL= APPLICATION_URL +"/login";
+    public static final String SIGN_UP_URL= APPLICATION_URL +"/signup";
+
 }
