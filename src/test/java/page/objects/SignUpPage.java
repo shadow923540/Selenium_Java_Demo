cc4f23e47050c86d7bf6c485520a78743dda7c08
Zuczkows <pz921@wp.pl>
2019-11-03 21:22:58 +0100

    add new webelement

--- src/test/java/page/objects/SignUpPage.java
+++ src/test/java/page/objects/SignUpPage.java
@@ -3,15 +3,76 @@ package page.objects;
 import io.qameta.allure.Step;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.FindBy;
+import waits.WaitForElement;
 
 public class SignUpPage extends BasePage {
 
     @FindBy(css=".google-button")
     private WebElement signUpWithGoogle;
 
+    @FindBy(css="#signup")
+    private WebElement signUpButton;
+
+    @FindBy(css="#error > p")
+    private WebElement invalidLoginPrompt;
+
+    @FindBy(css="#email")
+    private WebElement emailField;
+
+    @FindBy(css="#name")
+    private WebElement nameField;
+
+    @FindBy(css="#password")
+    private WebElement passwordField;
+
     @Step("Click on sign up with google")
     public GooglePage clickSignUpWithGoogle(){
         signUpWithGoogle.click();
         return new GooglePage();
     }
+
+    @Step("Check if signUpButton is clickable")
+    public boolean signUpIsClickable(){
+        WaitForElement.waitUntilElementIsVisible(signUpButton);
+        boolean isClickable = signUpButton.isEnabled();
+        return isClickable;
+    }
+
+    @Step("Type  email into emailField")
+    public SignUpPage typeIntoEmailField(String email){
+        WaitForElement.waitUntilElementIsVisible(emailField);
+        emailField.sendKeys(email);
+        return new SignUpPage();
+    }
+
+    @Step("Type name into nameField")
+    public SignUpPage typeIntoNameField(String name){
+        WaitForElement.waitUntilElementIsVisible(nameField);
+        nameField.sendKeys(name);
+        return new SignUpPage();
+    }
+
+    @Step("Type password into passwordField")
+    public SignUpPage typeIntoPasswordField(String passwd){
+        WaitForElement.waitUntilElementIsVisible(passwordField);
+        passwordField.sendKeys(passwd);
+        return new SignUpPage();
+    }
+
+    @Step("Click signup button")
+    public SignUpPage clickSignUpButton(){
+        WaitForElement.waitUntilElementIsClickable(signUpButton);
+        signUpButton.click();
+        return new SignUpPage();
+    }
+
+    @Step("Get invalidLoginPrompt message")
+    public String getinvalidLoginPrompt(){
+        WaitForElement.waitUntilElementIsVisible(invalidLoginPrompt);
+        String prompt = invalidLoginPrompt.getText();
+        return prompt;
+    }
+
+
+
 }
